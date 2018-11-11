25354f017dbabe8617764b30af98b93bbf47fc11
EXXETA-DE\joka <johannes.kahlhoefer@EXXETA.com>
2018-11-11 15:49:15 +0100

    aws lambda handler solidified

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,28 @@
 import {Callback, Context, Handler} from 'aws-lambda';
 import {processSlackSlashCommand} from './slack';
+import {SlackSlashCommandBody} from './model/slack-slash-command-body';
 
-const slack: Handler = (event: any, context: Context, callback: Callback) => {
-  callback(undefined, processSlackSlashCommand(event));
+const slack: Handler<any, string> = (event: any, context: Context, callback: Callback) => {
+  const slackCommand = getBodyAsSlackRequest(event.body);
+  const result = processSlackSlashCommand(slackCommand);
+  const response = {
+    statusCode: 200,
+    body: result,
+    headers: {'content-type': 'text/plain'},
+  };
+  callback(undefined, response);
 };
 
 export {slack};
+
+function getBodyAsSlackRequest(body: string): SlackSlashCommandBody {
+  const pairs = body.split('&');
+
+  const result = {};
+  pairs.forEach(function(pair) {
+    const kv = pair.split('=');
+    result[kv[0]] = decodeURIComponent(kv[1] || '');
+  });
+
+  return JSON.parse(JSON.stringify(result)) as SlackSlashCommandBody;
+}
